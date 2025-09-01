5d88f1bee6b96d0dbc5e8bbf99a38569e3970da1
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-09-01 12:21:30 +0300

    add characters

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,9 @@
+function Error({ message }) {
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<div className="text-2xl text-red-600">Error: {message}</div>
+		</div>
+	);
+}
+
+export default Error;
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+function Loading() {
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<div className="text-2xl">Loading data...</div>
+		</div>
+	);
+}
+export default Loading;
